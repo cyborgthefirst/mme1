c2b077879bb098aa42d3cbadfe08a240d7989f05
Benj_S <bschurian4@gmail.com>
2015-11-01 23:16:15 +0100

    made form better usable for js

--- popupNL.js
+++ popupNL.js
@@ -1,6 +1,9 @@
 document.getElementById('newsletter').addEventListener("click", openPopup, false);
 document.getElementById('popup-closebutton').addEventListener("click", closePopup, false);
-//document.getElementById('emailInput').addEventListener('change', checkPasswordValidity, false);
+
+var email = document.getElementById('emailInput');
+    
+document.getElementById('emailInput').addEventListener('keyup', checkPasswordValidity, false);
 
 function openPopup() {
     document.getElementById('emailpopup').style.display = 'block';
@@ -8,13 +11,15 @@ function openPopup() {
 function closePopup() {
     document.getElementById('emailpopup').style.display = 'none';
 }
-/*
+
 function checkPasswordValidity() {
-    var email = document.getElementById('emailInput');
-    if (email.validity.valueMissing) {
+    email.setCustomValidity('Please enter your email address.');
+    if (email.value=='') {
         email.setCustomValidity('Please enter your email address.');
     } else {
-        email.setCustomValidity('');
+        email.setCustomValidity('a');
     }        
 };
-*/   
+
+
+    
