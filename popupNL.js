dd0e8a011cc791030e10e36e01945dd1b687cab5
Benj_S <bschurian4@gmail.com>
2015-11-01 20:53:56 +0100

    small grammatical corrections added js-validation that does not work yet

--- popupNL.js
+++ popupNL.js
@@ -1,9 +1,20 @@
 document.getElementById('newsletter').addEventListener("click", openPopup, false);
 document.getElementById('popup-closebutton').addEventListener("click", closePopup, false);
+//document.getElementById('emailInput').addEventListener('change', checkPasswordValidity, false);
+
 function openPopup() {
     document.getElementById('emailpopup').style.display = 'block';
 }
 function closePopup() {
     document.getElementById('emailpopup').style.display = 'none';
 }
-
+/*
+function checkPasswordValidity() {
+    var email = document.getElementById('emailInput');
+    if (email.validity.valueMissing) {
+        email.setCustomValidity('Please enter your email address.');
+    } else {
+        email.setCustomValidity('');
+    }        
+};
+*/   
