3c74be2a8b89467791a546ce82f04e6fe0692975
Johannes Ebeling <johannes@ebelingfs.de>
2015-11-03 13:02:41 +0100

    Timeout feature added

---
+++ TpopupNL.js
@@ -0,0 +1,22 @@
+document.getElementById('Tpopup-closebutton').addEventListener("click", closePopup, false);
+
+document.addEventListener('change', startWait, false);
+
+var ival = setTimeout(showHomepage, 45000);
+
+function openPopup() {
+    document.getElementById('timeoutPopup').style.display = 'block';
+
+}
+function closePopup() {
+    document.getElementById('timeoutPopup').style.display = 'none';
+}
+
+function showHomepage(){
+    location.href = "index.html";
+}
+
+function startWait(){
+    clearTimeout(ival);
+    setTimeout(openPopup, 30000);
+    }
