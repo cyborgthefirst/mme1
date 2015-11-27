aa0ba9068bb831c8b60b55ea10de3a44a8296ac7
Benj_S <bschurian4@gmail.com>
2015-11-27 13:25:54 +0100

    coNameVali

--- js/contactFVali.js
+++ js/contactFVali.js
@@ -1,13 +1,66 @@
-var email = document.getElementById('contactEMail');
+var nameF = document.getElementById('contactName');
+var emailF = document.getElementById('contactEMail');
 
-//starts after the first iput into the email field
-email.addEventListener('keyup', checkEmailValidity, false);
+//on load set required fields validity false
+
+//starts after the first input into the email field
+nameF.addEventListener('keyup', checkNameValidity, false);
+
+//starts after the first input into the email field
+nameF.addEventListener('change', function(){
+    alert("d");
+}, false);
+
+
+//starts after the first input into the email field
+emailF.addEventListener('keyup', checkEmailValidity, false);
+
+
+function checkNameValidity() {
+    
+    alert("c");
+
+    //regex-validation
+    var error = "";
+    var nameVal = nameF.value;
+    
+    if (nameVal === ""){//name empty
+        nameF.setCustomValidity('The field "Your Name" is required. ');
+    }
+    
+    if (nameVal.match(/^(\w){4,30}$/)) {
+    } else {
+        error = error + "This name was not accepted. ";
+    	if(nameVal.match(/\W/)){
+            error = error + "Only names composed of alpha-numeric charachters (and '_') are accepted. ";
+        }else{
+            if(nameVal.match(/^\w{1,3}$/)){
+                error = error + "The Name is too short";
+            }
+            if(nameVal.match(/^\w{31,}$/)){
+                error = error + "The Name is too short";
+            }
+        }
+    }
+        
+    nameF.setCustomValidity(error);
+    
+    
+    
+    //css-manipulation
+    if (nameF.validity.valid) {
+        nameF.style.color = 'green';
+    } else {
+        nameF.style.color = 'red';
+    }        
+}
+
 
 function checkEmailValidity() {
     
     //regex-validation
     var error = "";
-    var emVal = email.value;
+    var emVal = emailF.value;
     
     if(emVal === ""){
         //set back state to neither wrong nor right
@@ -22,11 +75,11 @@ function checkEmailValidity() {
             } else {
                error = error + "The string before the @ -symbol may only contain &,+ ,- ,. ,' and alphanumeric charachters. ";
             }
-            if(emVal.value.match(/^[^@]*@[^@]*$/)){
+            if(emVal.match(/^[^@]*@[^@]*$/)){
             } else {
                error = error + "Your email address may only contain one @. ";
             }
-            if(emVal.value.match(/@[\w-]+(\.[a-zA-Z]+)+$/)){
+            if(emVal.match(/@[\w-]+(\.[a-zA-Z]+)+$/)){
             } else {
                error = error + "The domain of the email was not accepted. ";
             }            
@@ -35,15 +88,15 @@ function checkEmailValidity() {
         }
     }
         
-    email.setCustomValidity(error);
+    emailF.setCustomValidity(error);
     
     
     
     //css-manipulation
-    if (email.validity.valid) {
-        email.style.color = 'green';
+    if (emailF.validity.valid) {
+        emailF.style.color = 'green';
     } else {
-        email.style.color = 'red';
+        emailF.style.color = 'red';
     }        
 }
     
